9cede4dd25be631b79f107d3195a25d586001915
Samuele Zanetti <samuelezanetti1@gmail.com>
2026-03-30 22:52:51 +0200

    start advanced adversarial

--- master_run.py
+++ master_run.py
@@ -9,10 +9,10 @@ torch.cuda.empty_cache()
 
 script_to_launch = [
    # "baseline.py",
-     "adversarial_perturbations.py",
+    # "adversarial_perturbations.py",
     #"activation_steering.py",
     #"fast_steering.py",
-    # "advanced_adversarial.py",
+     "advanced_adversarial.py",
     # "logit_bias.py",
     # "summary.py",
     # "analisi_mec.py",
