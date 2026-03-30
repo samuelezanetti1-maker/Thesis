79f86f0687e8fc89b4ab5e62b18599288c0ab9e3
Samuele Zanetti <samuelezanetti1@gmail.com>
2026-03-30 08:25:25 +0200

    inizio deepseek from pc

--- config.py
+++ config.py
@@ -37,12 +37,12 @@ models_config = {
     #"Qwen/Qwen2.5-Coder-7B-Instruct": {"dtype": torch.float16},
     
     # Famiglia Llama (Meta)
-    "meta-llama/Llama-3.1-8B-Instruct": {"dtype": torch.float16},
-    "codellama/CodeLlama-7b-Instruct-hf": {"dtype": torch.float16},
+    #"meta-llama/Llama-3.1-8B-Instruct": {"dtype": torch.float16},
+    #"codellama/CodeLlama-7b-Instruct-hf": {"dtype": torch.float16},
     
     # Famiglia DeepSeek
-   # "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B": {"dtype": torch.float16},
-   # "deepseek-ai/deepseek-coder-6.7b-instruct": {"dtype": torch.float16}
+    "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B": {"dtype": torch.float16},
+    "deepseek-ai/deepseek-coder-6.7b-instruct": {"dtype": torch.float16}
 }
 
 
