66a2f62644b5bc87b01ec26362e5a6d3b3fb9595
Samuele Zanetti <s.zanetti-thesis@medooza-frontend.e4-cluster.lan>
2026-04-18 16:01:16 +0200

    end L2 steering

--- master_run.py
+++ master_run.py
@@ -21,9 +21,10 @@ script_to_launch = [
     # "snipe_steering.py",
     #"mass_diagnostic.py",
     #"plot_probing_mass.py",
-    "isomorfismo_attacco_bb_aa.py",
-    "isomorfismo_attacco_bb_ap.py",
-    "isomorfismo_attacco_bb_pj.py"
+    #"isomorfismo_attacco_bb_aa.py",
+    #"isomorfismo_attacco_bb_ap.py",
+    #"isomorfismo_attacco_bb_pj.py",
+    "L2_steering_graph.py"
 
 ]
 
