e42c3f2b56955b1367582597b46bc82e84261643
Samuele Zanetti <s.zanetti-thesis@medooza-frontend.e4-cluster.lan>
2026-05-04 16:46:36 +0200

    ti prego basta

--- logit_lens_semantic.py
+++ logit_lens_semantic.py
@@ -121,12 +121,12 @@ for model_name, config in models_config.items():
             )
         
         layer_per_modello = {
-        "Qwen/Qwen2.5-7B-Instruct": 18,
-        "Qwen/Qwen2.5-Coder-7B-Instruct": 18,
-        "meta-llama/Llama-3.1-8B-Instruct": 15,
-        "codellama/CodeLlama-7b-Instruct-hf": 13,
-        "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B": 19,
-        "deepseek-ai/deepseek-coder-6.7b-instruct": 16
+        "Qwen/Qwen2.5-7B-Instruct": 19,
+        "Qwen/Qwen2.5-Coder-7B-Instruct": 19,
+        "meta-llama/Llama-3.1-8B-Instruct": 16,
+        "codellama/CodeLlama-7b-Instruct-hf": 14,
+        "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B": 20,
+        "deepseek-ai/deepseek-coder-6.7b-instruct": 17
         }
 
         layer_locus = layer_per_modello.get(model_name)
--- master_run.py
+++ master_run.py
@@ -54,13 +54,13 @@ script_to_launch = [
     #"isomorfismo_ap_last.py",
     #"isomorfismo_pi_last.py",
     "logit_lens_semantic.py",
-    "logit_lens_last_layer.py",
+    #"logit_lens_last_layer.py",
     #"isomorfismo_steering.py",
     #"black_box_vuln_L2.py",
     #"vuln_steering_L2.py",
     #"evaluate_BB_change.py",
     #"change_steering.py",
-    "Logit_lens_sweep.py"
+    #"Logit_lens_sweep.py"
 
 ]
 
