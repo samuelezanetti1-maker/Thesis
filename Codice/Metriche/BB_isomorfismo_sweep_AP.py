2b33511852b3188a94c62b576f3bea7d5c880b52
Samuele Zanetti <samuelezanetti1@gmail.com>
2026-09-08 11:17:09 +0200

    Fix environment variable assignments and print statement

--- Codice/Metriche/BB_isomorfismo_sweep_AP.py
+++ Codice/Metriche/BB_isomorfismo_sweep_AP.py
@@ -6,8 +6,8 @@ import numpy as np
 import torch.nn.functional as F
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
-os.environ["HF_HOME"] = "/scratch_share/bislab/HF_HUB_CACHE/"
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = 
+os.environ["HF_HOME"] = 
 
 df_attacchi = pd.read_csv("CSV tesi/Fixed/risultati_attacco_adversarial_perturbation.csv")
 df_aa_succ = df_attacchi[df_attacchi['target_predetto_ap'] == 'Sicuro']
@@ -117,4 +117,4 @@ for model_name, config in models_config.items():
 os.makedirs("CSV tesi/Isomorfismo_Sweep", exist_ok=True)
 df_finale = pd.DataFrame(risultati_isomorfismo)
 df_finale.to_csv("CSV tesi/Isomorfismo_Sweep/sweep_isomorfismo_multi_vector_AP.csv", index=False)
-print("\n[+] Salvataggio completato in 'CSV tesi/Isomorfismo_Sweep/sweep_isomorfismo_multi_vector_AP.csv'")
+print("\n Salvataggio completato in 'CSV tesi/Isomorfismo_Sweep/sweep_isomorfismo_multi_vector_AP.csv'")
