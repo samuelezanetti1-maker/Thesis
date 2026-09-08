56a77caf8bbadf8cd21b21b2822c1d86018a4810
Samuele Zanetti <samuelezanetti1@gmail.com>
2026-09-08 11:20:22 +0200

    Remove environment variable configurations
    Removed environment variable settings for CUDA and HF_HOME.

--- Codice/Metriche/black_box_L2.py
+++ Codice/Metriche/black_box_L2.py
@@ -9,8 +9,8 @@ import matplotlib.pyplot as plt
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from config import models_config 
 
-os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
-os.environ["HF_HOME"] = "/scratch_share/bislab/HF_HUB_CACHE/"
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = 
+os.environ["HF_HOME"] = 
 os.makedirs("grafici_L2", exist_ok=True)
 
 def safe_norm(v1, v2):
